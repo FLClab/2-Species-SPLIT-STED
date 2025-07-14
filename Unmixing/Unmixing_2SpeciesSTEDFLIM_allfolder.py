f5d425abee8935438eaea1d10b957278d1814222
Andreanned3 <33556475+Andreanned3@users.noreply.github.com>
2025-07-14 17:45:10 -0400

    Update Unmixing_2SpeciesSTEDFLIM_allfolder.py
    documentation

--- Unmixing/Unmixing_2SpeciesSTEDFLIM_allfolder.py
+++ Unmixing/Unmixing_2SpeciesSTEDFLIM_allfolder.py
@@ -47,35 +47,38 @@ matplotlib.colormaps.register(cmap=cmap.reversed(), force=True)
 
 
 matplotlib.rcParams['axes.linewidth'] = 0.8
-
+#plt.style.use('dark_background')
 # -----------------------------------------------------------
 
-# Select the folders containing the control images and the mixed images
+# Select the folders containing the control images and the mixed images. Open a dialog box to select the folders.
 f1=easygui.diropenbox(default=os.path.expanduser("~Desktop"), title="Select folder containing the control images for the first fluorophore")
 f2=easygui.diropenbox(default=os.path.expanduser("~Desktop"), title="Select folder containing the control images for the second fluorophore")
 f3=easygui.diropenbox(default=os.path.expanduser("~Desktop"), title="Select folder containing the mixed images (the mixture of the two fluorophores)")
-
-#mapcomp = { 'Conf FLIM' : 'Confocal_561 {11}',
-#            'STED FLIM' :'STED 561 {11}' }
-
-mapcomp = { 'Conf FLIM' :'Conf_635P {2}', 'STED FLIM' : 'STED_635P {2}'}
-colors=['magenta',  'c' ,'springgreen']
-#labels=['Homer ATTO647N - Confocal','Bassoon Nanotag STAR635p - Confocal', 'Mixture']
-#labels = ['Bassoon Atto647N', 'Tubulin STAR 635P', 'Mélange']
-labels = ['Bassoon CF594', 'PSD95 STOrange', 'Mixture']
-#labels = ['Bassoon STARORANGE', 'MAP2 CF594', 'Mélange']
-#labels = ['Tubulin_LIVE610','Actin_SiR ', 'Mixture']
 filenamescontrol = [f1, f2]
 filenamemixed=f3
+
+
+
+# Labels for the legend
+# These labels correspond to the fluorophores used in the control images and the mixed images.
+labels = ['Bassoon CF594', 'PSD95 STOrange', 'Mixture']
+colors=['magenta',  'c' ,'springgreen']
+
+# Which channels to use (keys in the msr files, channel numbers in the tiff files)
+
+#keys=[1,1,1] # For Tiff files
+
+#keys=['STED640 {10}', 'STED640 {10}', 'STED640 {10}']
 #keys = ['STED_635P {2}', 'STED_635P {2}', 'STED_635P {2}']
 keys = ['STED 561 {11}', 'STED 561 {11}','STED 561 {11}']
-#keys=[1,1,1]
-#keys=['STED640 {10}', 'STED640 {10}', 'STED640 {10}']
-#plt.style.use('dark_background')
-powers=[[10,[0,0]],[20,[0,0]],[30,[0,0]],[40,[0,0]]]
-powers=[[5,[0,0]],[10,[0,0]],[15,[0,0]],[20,[0,0]]]
-savefolder=str(input("Name of Output folder: "))
 
+
+# List of STED depletion powers to analyze and the ID of the images to use as controls.
+powers=[[10,[0,0]],[20,[0,0]],[30,[0,0]],[40,[0,0]]]
+#powers=[[5,[0,0]],[10,[0,0]],[15,[0,0]],[20,[0,0]]]
+
+# Aske the user to input the output folder name. Create the output folder on the Desktop.
+savefolder=str(input("Name of Output folder: "))
 savefoldermain = os.path.join(os.path.expanduser("~/Desktop"), "Unmixing_"+savefolder+"_2Species")
 os.makedirs(savefoldermain, exist_ok=True)
 
@@ -86,18 +89,14 @@ img.set_visible(False)
 plt.colorbar(orientation="vertical")
 plt.savefig(os.path.join(savefoldermain,"Legend.pdf"),transparent=True)
 
+# Loop over the powers and process the images for each power.
 for power in powers:
     print(power)
 
     STEDPOWER=power[0]
+    # Create the output folder for the current power.
     savefolder = os.path.join( savefoldermain,"{}PercentSTED".format(STEDPOWER))
     os.makedirs(savefolder, exist_ok=True)
-
-
-
-
-# -----------------------------------------------------------
-#     Choisir un fichier msr parmi plusieurs
 
 
     msrfiles = []
@@ -112,8 +111,8 @@ for power in powers:
             images = glob.glob(path)
             print('There are ',len(images), ' tiff files in this folder')
             extension = ".tiff"
-        for imagei in images:
-            print(os.path.basename(imagei))
+        for i,imagei in enumerate(images):
+            print(i,os.path.basename(imagei))
         numim = power[1][k]
         image = images[numim]
         msrfiles.append(image)
@@ -122,8 +121,7 @@ for power in powers:
     path = os.path.join(filenamemixed, '*_{}PercentSTED{}'.format(STEDPOWER,extension))
     mixedimages = glob.glob(path)
 
-
-        #plt.style.use('dark_background')
+# Create Figure and axes for the phasor plot
     fig, ax = plt.subplots()
     fig4,ax_scatter = plt.subplots(figsize=(2,2))
 
@@ -138,29 +136,33 @@ for power in powers:
     x1 = r*numpy.cos(theta) + 0.5
     x2 = r*numpy.sin(theta)
     ax_scatter.plot(x1,x2, color = "k", ls = "--",linewidth=0.8)
-    CoM_x, CoM_y = [], []
+    
+
+    # Create a legend file 
     with open(os.path.join(savefolder,'legend.txt'),'w') as data:
         data.write("Controls\n")
-    scatterlist = []
 
+    scatterlist = []
+    CoM_x, CoM_y = [], []
     for i, msr in enumerate(msrfiles) :
         df = pd.DataFrame(columns=['x','y'])
         dg = pd.DataFrame(columns=['g', 's'])
+
+        # Write info about the control image used to to the legend file
         with open(os.path.join(savefolder,'legend.txt'),'a') as data:
             data.write("{}\t{}\t{}\n".format(labels[i],keys[i],msr))
+
+        # Load the control image and select the channel    
         imagemsr=load_image(msr)
         image1=select_channel(imagemsr,keys[i])
 
-        #image1 = imagemsr[keys[i]]
-        print(image1.shape)
-        #image1 =image1[10: -10, 10: -10,:]
         print(image1.shape)
         imsum = image1.sum(axis=2)
         imsum = imsum.astype('int16')
 
         seuil = get_foreground(imsum)
 
-
+        # Calulate the phasor coordinates for the foreground of the control image
         print("Caclulation for an image of shape", image1.shape, "...")
         params_dict["foreground_threshold"] = seuil
         params_dict["Nb_to_sum"] = image1.shape[2]
@@ -172,46 +174,47 @@ for power in powers:
         m, phi = to_polar_coord(df['x'], df['y'])
         g,s =polar_to_cart(m, phi)
         dg['g'], dg['s'] = g, s
+
+        # Calculate the centroid of the phasor distribution using KMeans clustering
         kmeans = KMeans(n_clusters = 1, init = 'k-means++', random_state = 42)
         y_kmeans = kmeans.fit_predict(dg)
         CoM_x.extend(kmeans.cluster_centers_[:, 0][:].tolist())
         CoM_y.extend(kmeans.cluster_centers_[:, 1][:].tolist())
-        #print('DENSITY ESTIMATION IS HARD WORK, BE PATIENT PLEASE')
-        #seaborn.kdeplot(x=g, y=s, ax=ax_scatter, color=colors[i], levels=[0.2, 0.4, 0.6, 0.8, 1.0], linewidths=2)
-        # ax_scatter.scatter(g, s, s=1, c=colors[i], alpha=0.1)
+
         a=ax_scatter.scatter(g, s, s=1, c=colors[i], alpha=0.1, label=labels[i],rasterized=True)
         scatterlist.append(a)
 
 
-    ##Calcul de Pn
+    ## Use the centroids of the phasor distributions to define the points Pn and P2
     Pn_x, Pn_y = CoM_x[0], CoM_y[0]
     P_n =numpy.array([Pn_x, Pn_y])
-    #norm = numpy.sqrt((CoM_x[1] - 0.5) ** 2 + (CoM_y[1] ** 2) )
+
     P2_x =CoM_x[1]
     P2_y =CoM_y[1]
     p2=numpy.array([P2_x,P2_y])
 
-    ## Droite entre Pn - p2
+    ## Create linear trajectory connecting Pn and p2
     xaxis = numpy.linspace(0, 1, 100)
     x2, y2 = [Pn_x, P2_x], [Pn_y,P2_y]
     m2, c2 = line_equation(x2, y2)
     y2 = m2 * xaxis + c2
+
+    # Plot the linear trajectory in the phasor plot and save the figure
     p2pnline = ax_scatter.plot([Pn_x, P2_x], [Pn_y, P2_y], c='dodgerblue')
     pnscatter = ax_scatter.scatter(Pn_x, Pn_y, s=50, c='gold')
     p2scatter = ax_scatter.scatter(P2_x, P2_y, s=50, c='gold')
     fig4.savefig(os.path.join(savefolder, "Phasor_2species_ControlsOnly.pdf"), transparent='True',
                     bbox_inches="tight",dpi=900)
+    
+    # Clear the scatter points and lines from the phasor plot 
     t = [scatter.remove() for scatter in scatterlist]
     lines = [mpatches.Patch(color=colors[j], label=labels[j]) for j in range(len(labels))]
-    #ax_scatter.legend(handles=lines, prop={'size': 20})
-
-    #ax_scatter.get_legend().remove()
     pnscatter.remove()
     p2scatter.remove()
     ax_scatter.lines[-1].remove()
 
 
-
+    # Loop over the mixed images and perform unmixing
     for m,mixedimage in enumerate(mixedimages):
         print("***********************************************************")
         print("Working on image number ",m," out of ",len(mixedimages))
@@ -219,6 +222,8 @@ for power in powers:
         d_melange = pd.DataFrame(columns=['g', 's'])
         df = pd.DataFrame(columns=['x', 'y'])
         dg = pd.DataFrame(columns=['g', 's'])
+
+        # Load the mixed image and select the channel
         imagemsr = load_image(mixedimage)
         image1 = select_channel(imagemsr, keys[2])
   
@@ -226,10 +231,10 @@ for power in powers:
         imsum = image1[:,:,10:111].sum(axis=2)
         imsum = imsum.astype('int16')
 
+        # Calculate the phasor distribution for the mixed image
         params_dict["foreground_threshold"] = 5
         params_dict["Nb_to_sum"] = image1.shape[2]
         print("foreground_threshold=", params_dict["foreground_threshold"])
-
 
         x, y, g_smoothed, s_smoothed, original_idxes = Median_Phasor(image1, params_dict, **params_dict)
         df['x'] = x.flatten()
@@ -240,6 +245,7 @@ for power in powers:
     
         d_melange['g'], d_melange['s'] = g, s
 
+        # Calculate the centroid of the phasor distribution using KMeans clustering
         kmeans = KMeans(n_clusters=1, init='k-means++', random_state=42)
         y_kmeans = kmeans.fit_predict(dg)
         CoM_x.extend(kmeans.cluster_centers_[:, 0][:].tolist())
@@ -247,28 +253,30 @@ for power in powers:
 
 
         p3 = d_melange[['g', 's']].to_numpy() 
+        # Perform unmixing using the phasor coordinates and the points Pn and P2
         imsum_flat_lin1,imsum_flat_lin2,Solve=unmix2species(p3, original_idxes,image1,P_n,p2)
         fraction1 = imsum_flat_lin2.copy()
         fraction2 = imsum_flat_lin1.copy()
         imsum_flat_lin1*= imsum
         imsum_flat_lin2*= imsum
+
+        # Plot the phasor distribution of the mixed image colored by the unmixing results and save the figure
         mixphasor = ax_scatter.scatter(g, s, s=1,c=Solve[1,:],cmap="cool",rasterized=True,label="Mixture")
 
         lines = [mpatches.Patch(color=colors[i], label=labels[i]) for i in range(len(labels))]
-    
-
         p2pnline = ax_scatter.plot([Pn_x, P2_x], [Pn_y, P2_y], c='dodgerblue')
         pnscatter = ax_scatter.scatter(Pn_x, Pn_y, s=50, c='gold')
         p2scatter = ax_scatter.scatter(P2_x, P2_y, s=50, c='gold')
         fig4.savefig(os.path.join(savefolder, "Phasor_2species_{}.pdf".format(os.path.basename(mixedimage).split(extension)[0])), transparent='True',
                         bbox_inches="tight",dpi=900)
-
+        
+        # Clear the scatter points and lines from the phasor plot
         pnscatter.remove()
         p2scatter.remove()
         ax_scatter.lines[-1].remove()
         mixphasor.remove()
 
-
+        # Create a figure to display the images and the unmixing results
         fig_im3, ax_im3 = plt.subplots(ncols=4,nrows=1,figsize=(12,8))
         ax_im3[0].axis('off')
         ax_im3[1].axis('off')
@@ -281,6 +289,8 @@ for power in powers:
         cbar2 =fig_im3.colorbar(imsum_flat5, ax=ax_im3[2],fraction=0.05, pad=0.01)
         imsum_flat6 =ax_im3[0].imshow(imsum, cmap='hot')
         cbar1 =fig_im3.colorbar(imsum_flat6, ax=ax_im3[0],fraction=0.05, pad=0.01)
+
+        
         overlayer = LifetimeOverlayer(fraction1, imsum/imsum.max(),  cname="coolspring")
         lifetime_rgb, cmap = overlayer.get_overlay(
             lifetime_minmax=(0., 1),
